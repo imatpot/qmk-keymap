ee680320a30507a7b3426e0d1d5e0508a8ecc8f3
Mladen Brankovic <root@brankovic.dev>
2021-07-22 20:05:47 +0200

    general: error handling in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
-# use zsa's instead of "official" repo
 git clone git@github.com:zsa/qmk_firmware.git
 
-# try NixOS today!
-ln -s qmk_firmware/shell.nix shell.nix
-echo "eval \"\$(lorri direnv)"\" > .envrc
-direnv allow
-lorri shell
+if [[ $(uname -v) == *NixOS* ]]
+then
+    echo "Info: NixOS detected! Attempting to create lorri environment..."
+    ln -s qmk_firmware/shell.nix shell.nix
 
-cd qmk_firmware
+    if command -v lorri &> /dev/null && command -v direnv &> /dev/null
+    then
+        echo "eval \"\$(lorri direnv)"\" > .envrc
+        direnv allow
+        lorri shell
+    else
+        echo "Failed setting up lorri environment: This script assumes you use lorri and direnv."
+    fi
+fi
 
-# installs chibios
-make git-submodule
+
+if command -v make &> /dev/null
+then
+    cd qmk_firmware
+    make git-submodule
+else
+    echo "Failed setting up qmk_firmware: make is not installed"
+fi
