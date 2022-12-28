99fad5ba4c1f4e3cc451e2701d700ab840382fa2
Mladen Brankovic <root@brankovic.dev>
2022-12-29 00:27:39 +0100

    build: add support for multiple keymaps

--- flake.nix
+++ flake.nix
@@ -15,49 +15,67 @@
 
   outputs = inputs @ { self, nixpkgs, utils, firmware }: utils.lib.eachDefaultSystem (system:
     let
-      pname = "qmk-keymap";
-      version = "1.0.0";
       keyboard = "planck/ez/glow";
-      keymap = "lucifer";
-      firmwareDir = "lib/qmk_firmware";
-      keymapDir = "${firmwareDir}/keyboards/${keyboard}/keymaps";
-      keymapFile = "${builtins.replaceStrings ["/"] ["_"] keyboard}_${keymap}.bin";
       pkgs = nixpkgs.legacyPackages.${system};
       qmkExe = "${pkgs.qmk}/bin/qmk";
     in
-    {
-      packages.default = pkgs.stdenv.mkDerivation {
-        inherit pname version;
+    rec {
+      packages = rec {
+        cerberus = mkKeymap {
+          keymap = "cerberus";
+          version = "1.0.0";
+        };
 
-        src = ./.;
-        nativeBuildInputs = with pkgs; [ qmk ];
+        lucifer = mkKeymap {
+          keymap = "lucifer";
+          version = "1.0.0";
+        };
 
-        SKIP_GIT = true;
-        BUILD_DIR = ./.;
-
-        installPhase = ''
-          mkdir -p $out/{bin,lib,share}
-
-          # Copy keymap into the firmware
-
-          cp -r --no-preserve=all ${firmware} $out/${firmwareDir}
-          mkdir -p $out/${keymapDir}
-          cp -r --no-preserve=all keymap $out/${keymapDir}/${keymap}
-
-          # Build the layout
-
-          find $out -name "*.sh" -exec chmod +x {} +
-          make -C $out/${firmwareDir} ${keyboard}:${keymap}
-          ln -s $out/${firmwareDir}/.build/${keymapFile} $out/share/${keymapFile}
-
-          # Create the flash script
-          # TODO: find a way to temporarily set user.qmk_home or use a specific firmware path because this overwrites the globally set user.qmk_home
-
-          echo "${qmkExe} setup --yes --home $out/${firmwareDir}" >> $out/bin/${pname}
-          echo "${qmkExe} flash $out/share/${keymapFile} --bootloader dfu-util" >> $out/bin/${pname}
-          chmod +x $out/bin/${pname}
-        '';
+        default = cerberus;
       };
+
+      mkKeymap = { keymap, version }:
+        let
+          pname = "qmk-keymap-${keymap}";
+        in
+        pkgs.stdenv.mkDerivation {
+          inherit pname version;
+
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ qmk ];
+
+          SKIP_GIT = true;
+          BUILD_DIR = ./.;
+
+          installPhase =
+            let
+              firmwareDir = "lib/qmk_firmware";
+              keyboardDir = "${firmwareDir}/keyboards/${keyboard}";
+              keymapBin = "${builtins.replaceStrings ["/"] ["_"] keyboard}_${keymap}.bin";
+            in
+            ''
+              mkdir -p $out/{bin,lib,share}
+
+              # Copy keymap into the firmware
+
+              cp -r --no-preserve=all ${firmware} $out/${firmwareDir}
+              mkdir -p $out/${keyboardDir}
+              cp -r --no-preserve=all keymaps $out/${keyboardDir}
+
+              # Build the layout
+
+              find $out -name "*.sh" -exec chmod +x {} +
+              make -C $out/${firmwareDir} ${keyboard}:${keymap}
+              ln -s $out/${firmwareDir}/.build/${keymapBin} $out/share/${keymapBin}
+
+              # Create the flash script
+              # TODO: find a way to temporarily set user.qmk_home or use a specific firmware path because this overwrites the globally set user.qmk_home
+
+              echo "${qmkExe} setup --yes --home $out/${firmwareDir}" >> $out/bin/${pname}
+              echo "${qmkExe} flash $out/share/${keymapBin} --bootloader dfu-util" >> $out/bin/${pname}
+              chmod +x $out/bin/${pname}
+            '';
+        };
     }
   );
 }
